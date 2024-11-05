62362171c356c70933e91b703eeacfa3621ca4a9
P-Kaden <kaden.t.parkin@gmail.com>
2024-11-05 15:50:39 -0700

    All the Spring Boot Things

---
+++ springBoot/src/main/java/dev/dungeonMaker/DungeonMakerApplication.java
@@ -0,0 +1,13 @@
+package dev.dungeonMaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonMakerApplication.class, args);
+	}
+
+}
---
+++ springBoot/src/test/java/dev/dungeonMaker/DungeonMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dungeonMaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
